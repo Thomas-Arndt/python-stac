012f9d8a474fd58261965d22477b9d366abdfe18
Thomas Arndt <arndtt42@gmail.com>
2021-11-29 11:12:00 -0800

    Assignment to predict the output of given function code blocks

---
+++ fundamentals/fundamentals/functions_basics_i.py
@@ -0,0 +1,133 @@
+# #1
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+# This will print "5" to the console
+
+# #2
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# This will have an error of 'number_of_days_in_a_week_silicon_or_triangle_sides' is not defined
+
+# #3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+# This will print "5" to the console
+
+# #4
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+# This will print "5" to the console
+
+# #5
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+# This will print "5" and "none" to the console
+
+# #6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+# This will print "3", "5", and an eror that two undefined amounts cannot be added
+
+# #7
+def concatenate(b,c):
+    return str(b)+str(c)
+="support function python from-rainbow">print(concatenate(2,5))
+# This will return an error with invalid syntax at line 43
+
+#8
+def number_of_oceans_or_fingers_or_continents():
+    b=100
+    print(b)
+    if b<10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+# This will print "100" and "10" to the console
+
+# #9
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# This will print "7", "14", and "21" to the console
+
+# #10
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+# This will print "8" to the console
+
+# #11
+b = 500
+print(b)
+def foobar():
+    b ="keyword operator from-rainbow">= 300
+    print(b)
+print(b)
+foobar()
+print(b)
+# This will print "500", "500", "True, "500" to the console
+
+# #12
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+# This will print "500", "500", "300", "500" to the console
+
+# #13
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+# This will print "500", "500", "300", "300" to the console
+
+# #14
+# def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+# This will print "1", "3", "2" to the console
+
+# #15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
+# THis will print "1", "3", "5", "10" to the console
