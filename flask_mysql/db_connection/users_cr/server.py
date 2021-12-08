2e1a09fa534cc01227727bdd7437c2285d12c793
Thomas Arndt <arndtt42@gmail.com>
2021-12-07 16:21:47 -0800

    Assignment to create a simple webpage that utilizes the create and read aspects of a CRUD application

---
+++ flask_mysql/db_connection/users_cr/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, request
+from users import Users
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/display')
+def users():
+    users=Users.get_all()
+    return render_template("users.html", all_users=users)
+
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    print(request.form)
+    data={
+        "fname": request.form['fname'],
+        "lname": request.form['lname'],
+        "email": request.form['email']
+    }
+    Users.create(data)
+    return redirect('/display')
+
+while __name__=="__main__":
+    app.run(debug=True)
